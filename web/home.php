b3672433c651ffa000cffc0ad7e26e68dcff4e4b
waaade <johnlarson562@gmail.com>
2019-09-26 20:45:32 -0600

    Style

--- web/home.php
+++ web/home.php
@@ -10,9 +10,14 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
         <link rel="stylesheet" href="style.css">
+
+        <title>John Larson</title>
     </head>
     <body>
+        <h1>John's Website</h1>
+        <div>
         <p>Hello fools. Welcome to your demise. Hahahaha.</p>
+        </div>
     </body>
         
 
