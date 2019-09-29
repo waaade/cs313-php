7ab631d3d7b0172898383564bea8ce5da10247b7
waaade <johnlarson562@gmail.com>
2019-09-28 19:35:12 -0600

    Style adjustments

--- web/home.php
+++ web/home.php
@@ -17,13 +17,13 @@
     <body>
         <h1>John's Website</h1>
         <div>
+        <img class="img-responsive" src="home.jpeg">
         <p>Welcome to my website. I'm John, a Brigham Young University-Idaho student 
         majoring in software engineering.</p>
         <p>I'm from the beautiful land of Colorado, USA.</p>
         <p>Some things I like to do aside from software development are writing, playing guitar,
         playing video games and reading manga. I also like hiking or doing anything outdoors.</p>
         <p>Check out the projects on this website.</p> 
-        <img class="img-responsive" src="home.jpeg">
         </div>
     </body>
         
