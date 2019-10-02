27b304413855bea430fa071166111d2477fcfaa3
waaade <johnlarson562@gmail.com>
2019-10-02 17:13:33 -0600

    Added browse.php

---
+++ web/prove03/browse.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>The Soda Store</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <h1>The Soda Store</h1>
+    <table>
+        <tr>
+            <td>Coke</td>
+            <td>$1,000,000</td>
+            <td><button>Add To Cart</button></td>
+        </tr>
+        <tr>
+            <td>Sprite</td>
+            <td>A million</td>
+            <td><button>Add To Cart</button></td>
+            <td><button>Remove</button></td>
+        </tr>
+        <tr>
+            <td>Stuff</td>
+            <td>I'm stuff</td>
+            <td><button>Your girlfriend's awesome</button></td>
+            <td><button>Remove</button></td>
+        </tr>
+</body>
+</html>
