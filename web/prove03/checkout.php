fab7b5bcf763c63f60d5ea5b00ade53521ae6576
waaade <johnlarson562@gmail.com>
2019-10-05 22:35:26 -0600

    Style. Added form.

---
+++ web/prove03/checkout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+    <body>
+    <div>
+        <form action="confirmation.php">
+        Name: <input type="text" name="name">
+        Address: <input type="text" name="address">
+        State: <input type="text" name="website">
+        Zip: <input type="number" name="zip">
+        <input type="submit" value="Submit">
+        </form>
+    </div>
+    </body>
+</html>
