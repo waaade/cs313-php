56e509362a0a6352b1d5a5dc82b2ff5eb277fde3
waaade <johnlarson562@gmail.com>
2019-10-05 22:47:53 -0600

    Stylin

--- web/prove03/checkout.php
+++ web/prove03/checkout.php
@@ -3,14 +3,28 @@ session_start();
 ?>
 <!DOCTYPE HTML>
 <html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Enter Address</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+    <h1>The Soda Store</h1>
+    <h2>Complete Your Order</h2>
     <body>
     <div>
         <form action="confirmation.php">
-        Name: <input type="text" name="name">
-        Address: <input type="text" name="address">
-        State: <input type="text" name="website">
-        Zip: <input type="number" name="zip">
-        <input type="submit" value="Submit">
+        Name: <input type="text" name="name"><br>
+        Address: <input type="text" name="address"><br>
+        State: <input type="text" name="website"><br>
+        Zip: <input type="number" name="zip"><br>
+        <input type="submit" value="Submit"><br>
         </form>
     </div>
     </body>
