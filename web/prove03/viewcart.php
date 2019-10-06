4fcedfdbe4a097c5cdd2374f05f7daecf8c16902
waaade <johnlarson562@gmail.com>
2019-10-05 21:59:57 -0600

    More HTML content and style

--- web/prove03/viewcart.php
+++ web/prove03/viewcart.php
@@ -3,11 +3,20 @@ session_start();
 ?>
 <!DOCTYPE HTML>
 <html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Your Cart</title>
+</head>
 <body>
-<?php
-foreach($_SESSION['cart'] as $value) {
-    echo $value . "<br>";
-}
-?>
-</body>
+    <div>
+    <h1>The Soda Store</h1>
+    <h2>Your Cart</h2>
+    <?php
+    foreach($_SESSION['cart'] as $value) {
+        echo $value . "<br>";
+    }
+    ?>
+    <a href="checkout.php">Checkout</a>
+    </div>
+    </body>
 </html>
