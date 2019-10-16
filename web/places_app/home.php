09a48d53e8af4df4a06b7583ea29dc0476fb7373
waaade <johnlarson562@gmail.com>
2019-10-16 14:38:28 -0600

    Basic html of places_app/home.php

---
+++ web/places_app/home.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+        <title>Rexburg Places</title>
+    </head>
+    <body>
+        <h1>Rexburg Places</h1>
+        <p>Know the best places to eat, shop, and have fun in and around Rexburg, Idaho and give advice to the community.</p>
+        <div>
+        <h2>See Places in these categories:</h2>
+        <ul>
+            <li><a>Restaurants</a></li>
+            <li><a>Parks/Trails</a></li>
+            <li><a>Recreation</a></li>
+            <li><a>Stores</a></li>
+        </ul>
+        <br>
+        <h2>Search for a Place</h2>
+        </div>
+    </body>
+</html>
