bcdfdbf556515f432f2c4e53a9a94a8dd95aac93
waaade <johnlarson562@gmail.com>
2019-10-18 16:45:53 -0600

    possibly fixed issue with connecting to db

--- web/places_app/typesearch.php
+++ web/places_app/typesearch.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require "dbConnect.php";
+$db = get_db();
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,7 +13,7 @@ session_start();
     $type = htmlspecialchars(trim($_POST['type']));
     echo $type;
     foreach ($db->query("SELECT * FROM places 
-    WHERE places_type=(SELECT types_id FROM types WHERE name='Restaurants')", PDO::FETCH_ASSOC) as $row)
+    WHERE places_type=(SELECT types_id FROM types WHERE name='$type')", PDO::FETCH_ASSOC) as $row)
     {
         echo '<p>' . $row['name'] . '</p>';
     }
