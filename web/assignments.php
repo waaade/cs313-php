d88c965b4ec7649bc5e18d671811541a01f4057e
waaade <johnlarson562@gmail.com>
2019-10-18 20:51:02 -0600

    Updated assignments page

--- web/assignments.php
+++ web/assignments.php
@@ -2,7 +2,28 @@
 <html>
     <head>
         <title>John Larson Assignments</title>
-    </head>
-    <h1>Coming Soon</h1>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="home.php">Home</a></li>
+                    <li><a href="assignments.php">Assignments</a></li>
+                </ul>
+            </div>
+    <h1>My Assignments</h1>
+    <div>
+    <ul>
+    <li><a href="prove03/browse.php">Shopping Cart</a></li>
+    <li><a href="places_app/home.php">Local Reviews Application</a></li>
+    </div>
 </html>
